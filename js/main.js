e1a0528350a702b1ae3e4f7b5df39cff008b35cd
synesdeija <esemde@pm.me>
2022-04-14 15:31:15 -0500

    it works! solved API bug and added decor

--- js/main.js
+++ js/main.js
@@ -1,45 +1,17 @@
 document.querySelector('button').addEventListener('click', chuckle)
 
-const chuckle = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
-        'X-RapidAPI-Key': 'b61d271c4amsh09f72ade5cfdd6cp15ff7djsn315312e11c02'
-    }
-};
+function chuckle() {
 
-fetch('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random', chuckle)
-    .then(response => response.json())
+    const url = `https://api.chucknorris.io/jokes/random`
 
-.then(response => console.log(response))
-document.querySelector('h3').innerText = response
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+            console.log(data)
+            document.querySelector('h3').innerText = data.value
 
-
-
-
-    .catch(err => console.error(err));
-
-
-
-
-
-// document.querySelector('button').addEventListener('click', chuckle)
-
-// function chuckle() {
-
-
-//     fetch(``)
-//         .then(res => res.json()) //saying:parse response as json
-//         .then(data => {
-//             console.log(data.drinks[0])
-//             document.querySelector('h2').innerText = data.drinks[0].strDrink
-//             document.querySelector('img').src = data.drinks[0].strDrinkThumb
-//             document.querySelector('h3').innerText = data.drinks[0].strInstructions
-
-//         })
-
-//     .catch(err => {
-//         console.log(`error ${err}`)
-//     })
-// }
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+}
